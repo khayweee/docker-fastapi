bf73d5b3373c7612c3ead12a85301dbbf09d15ed
khayweee <khayweeang@gmail.com>
2021-05-17 22:44:16 +0800

    connected to postgres db

--- app/routers/city/City_class.py
+++ app/routers/city/City_class.py
@@ -4,8 +4,14 @@ from tortoise import fields
 from tortoise.models import Model
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
+import os
 
 from .city_api import router
+
+HOST = os.environ.get('DB_HOST'),
+NAME = os.environ.get('DB_NAME'),
+USER = os.environ.get('DB_USER'),
+PASSWORD = os.environ.get('DB_PASS'),
 
 db = []
 
@@ -31,10 +37,11 @@ class City(Model):
 City_Pydantic = pydantic_model_creator(City, name='City')
 # CityIn will be used for data coming in
 CityIn_Pydantic = pydantic_model_creator(City, name='CityIn', exclude_readonly=True)
-
+print(f'postgres://{USER[0]}:{PASSWORD[0]}@{HOST[0]}:5432/{NAME[0]}')
 register_tortoise(
     router,
-    db_url='sqlite://database/city_db.sqlite3',
+    #db_url='sqlite://database/city_db.sqlite3',
+    db_url=f'postgres://{USER[0]}:{PASSWORD[0]}@{HOST[0]}:5432/{NAME[0]}',
     modules={'models': ['routers.city.City_class']},
     generate_schemas=True
 )
